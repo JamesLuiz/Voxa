44e3fc366e577ccb6cc268542c4e3fec553abee6
JamesLuiz <jamesluiz116@gmail.com>
2025-11-01 16:11:55 +0100

    fixing mongo connection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,8 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['http://localhost:8080'],
     credentials: true,
   });
 
